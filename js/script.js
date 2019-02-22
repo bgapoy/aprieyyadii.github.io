900320750358f98832ef4fcfd298952f50b19148
Apriyadi Abdullah <aprieyyadii@gmail.com>
2019-02-22 20:43:11 +0700

    baru

--- js/script.js
+++ js/script.js
@@ -6,7 +6,24 @@ $(document).ready(function(){
 					var $target = $(target);
 
 					$('html, body').animate({
-						'scrollTop': $target.offset().top - 50
-					}, 1000, 'swing');
+						'scrollTop': $target.offset().top -50
+					}, 1000, 'easeInOutExpo');
 				});
 			});
+
+
+
+
+$(window).scroll(function(){
+	var wscroll = $(this).scrollTop();
+
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px , '+wscroll/5+'%)'
+	});
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px , '+wscroll/2+'%)'
+	});
+	$('.jumbotron p').css({
+	'transform' : 'translate(0px , '+wscroll/1.2+'%)'
+	});
+});
