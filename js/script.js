ef2dd9a4ddd9ca110abbcad00427e7e8f96fa094
Apriyadi Abdullah <aprieyyadii@gmail.com>
2019-02-22 16:09:21 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 					var $target = $(target);
 
 					$('html, body').animate({
-						'scrollTop': $target.offset().top -50
+						'scrollTop': $target.offset().top - 100
 					}, 1000, 'swing');
 				});
 			});
